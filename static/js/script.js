edc572bac8665873d121c9161ec37a47838fa0f9
Yixin <yxsong525@gmail.com>
2023-09-26 23:43:30 +0200

    enhancement: UI change after session ended by backend

--- static/js/script.js
+++ static/js/script.js
@@ -3,8 +3,10 @@ document.addEventListener("DOMContentLoaded", function() {
     // ---------------- USER INTERACTION ----------------
     var conversation = document.getElementById("conversation");
     var userInput = document.getElementById("user-input");
+    var buttonContainer = document.getElementById("button-container");
     var sendButton = document.getElementById("send-button");
     var closeButton = document.getElementById("close-button");
+    var newSessionHint = document.getElementById("new-session-hint");
 
 
     function setFocusOnInput() {
@@ -87,14 +89,41 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 
 
+    function chatEndedUIChange() {
+        lockInputField();
+        HideButtonContainer();
+    }
+
+    function chatStartUIChange() {
+        hideNewSessionHint();
+        unlockInputField();
+        setFocusOnInput();
+        showButtonContainer();
+    }
+
     function lockInputField() {
         userInput.disabled = true;
     }
 
-
     function unlockInputField() {
         userInput.disabled = false;
     }
+
+    function showButtonContainer() {
+        buttonContainer.style.display = 'block';
+    }
+
+    function HideButtonContainer() {
+        buttonContainer.style.display = 'none';
+    }
+
+    function showNewSessionHint() {
+       newSessionHint.style.display = 'block';
+    }
+
+    function hideNewSessionHint() {
+        newSessionHint.style.display = 'none';
+     }
 
     function saveRecords() {
         fetch('/save-records', {
@@ -108,8 +137,11 @@ document.addEventListener("DOMContentLoaded", function() {
             method: 'GET'
         });
         console.log('Session refreshed');
-        unlockInputField();
-        setFocusOnInput();
+        refreshNewSession();
+    }
+
+    function refreshNewSession() {
+        chatStartUIChange();
     }
 
 
@@ -152,9 +184,10 @@ document.addEventListener("DOMContentLoaded", function() {
         type = instruction.type
         if (type == 'refresh_session_timer') {
             addMessage(instruction.goodbye_msg, 'assistant');
+            chatEndedUIChange();
             saveRecords();
-            lockInputField();
             setTimeout(close_session, instruction.timer * 1000);  // TODO: clear timer if second call here
+            showNewSessionHint();
         }
         else {
             console.error('Unknown instruction:', type);
